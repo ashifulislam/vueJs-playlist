f493448c54c6ec36ec7d6cde36f3798cd9368a6c
Ashiful Islam Prince <princeashifulislam@gmail.com>
2020-10-04 12:45:09 +0600

    updated

--- app.js
+++ app.js
@@ -5,16 +5,54 @@ new Vue({
     data:{
         //added to this vue instances
         position:'is here',
+        age:'',
+        age_update:20,
+        x:0,
+        y:0,
+        a:0,
+        b:0,
         job:'Software Engineering',
+        name:'',
+        website:'https://web.facebook.com/?_rdc=1&_rdr',
+        website2:'https://github.com/ashifulislam?tab=repositories',
+        website_tag:'<a href="https://twitter.com/ashiful_prince">Twitter Profile</a>'
     },
     methods:{
         showName:function(last_name){
 
             return 'My name is ashiful islam '+last_name+' '+this.position
+        },
+        //Perform the addition here
+        add_age:function(inc){
+            this.age+=inc;
+        },
+        //Perform the subtraction here
+        subtract_age(dec) {
+            this.age -= dec;
+        },
+        UpdateXY:function(event){
+            this.x=event.offsetX;
+            this.y=event.offsetY;
+        },
+        click:function(){
+            alert('You clicked me');
+        },
+        log_name:function(){
+        console.log("You entered you name babu");
+        },
+        log_age:function() {
+            console.log("You entered you age babu");
+        },
+        add_to_A:function(){
+           return this.a + this.age_update;
+        },
+        add_to_B:function(){
+          return  this.b + this.age_update;
         }
 
+
     },
-    website:'https://web.facebook.com/?_rdc=1&_rdr'
+
 
 
 });
--- index.php
+++ index.php
@@ -10,7 +10,39 @@
      </body>
     <div id="vue-app">
         <h1>Data binding</h1>
-        <a href="website"></a>
+        <a v-bind:href="website">Facebook</a><br><br>
+        <a v-bind:href="website2">Prince's_GitHub_Profile</a>
+        <br><br>
+        <input type="text" name="first_name" v-bind:value="name"/>
+        <p v-html="website_tag"></p>
+        <h1>Events</h1>
+        <button v-on:click.once="add_age(1)">Add a year</button>
+        <br><br>
+        <button @click="subtract_age(1)">Subtract from year</button>
+        <br><br>
+        <button @dblclick="add_age(10)">Add 10 year</button>
+        <br><br>
+        <button @dblclick="subtract_age(10)">Subtract 10 year</button>
+        <p>I am {{age}} years old</p>
+        <div id="prince_canvas" @mousemove="UpdateXY">
+            {{x}},{{y}}
+        </div>
+        <br>
+        <a v-on:click.prevent="click" href="https://www.w3schools.com/">W3schools.com</a>
+        <h1>KEYBOARD EVENTS</h1>
+        <label>Name</label>
+        <input type="text" name="name" v-model="name">
+        <span>{{name}}</span>
+        <lable>Age </lable>
+        <input type="text" name="age" v-model="age">
+        <span>{{age}}</span>
+        <h1>WORKING WITH COMPUTED PROPERTIES</h1>
+        <button v-on:click="a++">Add to A</button>
+        <button v-on:click="b++">Add to B</button>
+        <p>A -{{a}}</p>
+        <p>B -{{b}}</p>
+        <p>Age_update + A {{add_to_A()}}</p>
+        <p>Age_update + B {{add_to_B()}}</p>
 
     </div>
      <script src="app.js"></script>
